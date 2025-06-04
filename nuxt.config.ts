72a8eb04ed6724d86646d38bbe91181ac1b66928
Anbraten <anton@ju60.de>
2025-06-04 16:48:37 +0200

    :fire: enable source maps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
+  sourcemap: true,
   app: {
     head: {
       title: 'BugSlide',
