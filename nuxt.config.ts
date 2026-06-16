419a7dd30b6f22166ecac5eb6c5cfd406a2fcd8a
Anbraten <anton@ju60.de>
2026-06-16 19:59:00 +0200

    📦 update packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: '2026-06-16',
   devtools: { enabled: true },
   modules: ['@nuxt/icon', '@nuxtjs/color-mode', '@nuxtjs/tailwindcss'],
   colorMode: {
